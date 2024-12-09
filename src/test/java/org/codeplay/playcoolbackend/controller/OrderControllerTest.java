ed298d6ca67748bc28746f2615b17e7c30b99412
Mason ma <MAMA14@oocl.com>
2024-12-09 16:25:28 +0800

    Revert "refactor:remove order controller test"
    This reverts commit c32308da

--- src/test/java/org/codeplay/playcoolbackend/controller/OrderControllerTest.java
+++ src/test/java/org/codeplay/playcoolbackend/controller/OrderControllerTest.java
@@ -35,84 +35,84 @@ public class OrderControllerTest {
     @MockitoBean
     private OrderService orderService;
 
-//    @Test
-//    public void given_useId_when_test_getAllOrders_byUserId_then_list_orderResponseDto() throws Exception {
-//        OrderResponseDto order = new OrderResponseDto();
-//        order.setOrderId(1L);
-//        order.setOrderStatus(OrderStatus.PENDING);
-//        order.setPaymentStatus(PaymentStatus.PENDING);
-//        order.setPrice(100);
-//        order.setPaymentMethod("paymentMethod");
-//        order.setCreatedAt(new Date());
-//
-//        when(orderService.getOrdersByUserId(1L)).thenReturn(List.of(order));
-//        mockMvc.perform(get("/order/getAll"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//    }
-//
-//    @Test
-//    public void givenOrderRequestDto_whenCreateOrder_thenOrderResponseDto() throws Exception {
-//        OrderRequestDto orderRequestDto = new OrderRequestDto();
-//        orderRequestDto.setUserId(1L);
-//        orderRequestDto.setConcertId(1L);
-//        orderRequestDto.setPrice(100);
-//        orderRequestDto.setPaymentMethod("paymentMethod");
-//
-//        OrderResponseDto order = new OrderResponseDto();
-//        order.setOrderId(1L);
-//        order.setOrderStatus(OrderStatus.PENDING);
-//        order.setPaymentStatus(PaymentStatus.PENDING);
-//        order.setPrice(100);
-//        order.setPaymentMethod("paymentMethod");
-//        order.setCreatedAt(new Date());
-//
-//        when(orderService.createOrder(orderRequestDto)).thenReturn(order);
-//        mockMvc.perform(post("/order/create")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(new ObjectMapper().writeValueAsString(orderRequestDto)))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//    }
-//
-//
-//    @Test
-//    public void givenPaymentRequestDto_whenPayOrder_thenOrderResponseDto() throws Exception {
-//        PaymentRequestDto paymentRequestDto = new PaymentRequestDto();
-//        paymentRequestDto.setOrderId(1L);
-//        paymentRequestDto.setPaymentMethod("paymentMethod");
-//
-//        OrderResponseDto order = new OrderResponseDto();
-//        order.setOrderId(1L);
-//        order.setOrderStatus(OrderStatus.UNUSED);
-//        order.setPaymentStatus(PaymentStatus.COMPLETED);
-//        order.setPrice(100);
-//        order.setPaymentMethod("paymentMethod");
-//        order.setCreatedAt(new Date());
-//
-//        when(orderService.payOrder(paymentRequestDto)).thenReturn(order);
-//        mockMvc.perform(put("/order/pay")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(new ObjectMapper().writeValueAsString(paymentRequestDto)))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//    }
-//
-//    @Test
-//    public void givenOrderId_whenUseOrder_thenOrderResponseDto() throws Exception {
-//        OrderResponseDto order = new OrderResponseDto();
-//        order.setOrderId(1L);
-//        order.setOrderStatus(OrderStatus.COMPLETED);
-//        order.setPaymentStatus(PaymentStatus.COMPLETED);
-//        order.setPrice(100);
-//        order.setPaymentMethod("paymentMethod");
-//        order.setCreatedAt(new Date());
-//
-//        when(orderService.useOrder(1)).thenReturn(order);
-//        mockMvc.perform(put("/order/use/1"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
-//    }
+    @Test
+    public void given_useId_when_test_getAllOrders_byUserId_then_list_orderResponseDto() throws Exception {
+        OrderResponseDto order = new OrderResponseDto();
+        order.setOrderId(1L);
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setPaymentStatus(PaymentStatus.PENDING);
+        order.setPrice(100);
+        order.setPaymentMethod("paymentMethod");
+        order.setCreatedAt(new Date());
+
+        when(orderService.getOrdersByUserId(1L)).thenReturn(List.of(order));
+        mockMvc.perform(get("/order/getAll"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void givenOrderRequestDto_whenCreateOrder_thenOrderResponseDto() throws Exception {
+        OrderRequestDto orderRequestDto = new OrderRequestDto();
+        orderRequestDto.setUserId(1L);
+        orderRequestDto.setConcertId(1L);
+        orderRequestDto.setPrice(100);
+        orderRequestDto.setPaymentMethod("paymentMethod");
+
+        OrderResponseDto order = new OrderResponseDto();
+        order.setOrderId(1L);
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setPaymentStatus(PaymentStatus.PENDING);
+        order.setPrice(100);
+        order.setPaymentMethod("paymentMethod");
+        order.setCreatedAt(new Date());
+
+        when(orderService.createOrder(orderRequestDto)).thenReturn(order);
+        mockMvc.perform(post("/order/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(orderRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+
+    @Test
+    public void givenPaymentRequestDto_whenPayOrder_thenOrderResponseDto() throws Exception {
+        PaymentRequestDto paymentRequestDto = new PaymentRequestDto();
+        paymentRequestDto.setOrderId(1L);
+        paymentRequestDto.setPaymentMethod("paymentMethod");
+
+        OrderResponseDto order = new OrderResponseDto();
+        order.setOrderId(1L);
+        order.setOrderStatus(OrderStatus.UNUSED);
+        order.setPaymentStatus(PaymentStatus.COMPLETED);
+        order.setPrice(100);
+        order.setPaymentMethod("paymentMethod");
+        order.setCreatedAt(new Date());
+
+        when(orderService.payOrder(paymentRequestDto)).thenReturn(order);
+        mockMvc.perform(put("/order/pay")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(paymentRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void givenOrderId_whenUseOrder_thenOrderResponseDto() throws Exception {
+        OrderResponseDto order = new OrderResponseDto();
+        order.setOrderId(1L);
+        order.setOrderStatus(OrderStatus.COMPLETED);
+        order.setPaymentStatus(PaymentStatus.COMPLETED);
+        order.setPrice(100);
+        order.setPaymentMethod("paymentMethod");
+        order.setCreatedAt(new Date());
+
+        when(orderService.useOrder(1)).thenReturn(order);
+        mockMvc.perform(put("/order/use/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
 
 
 }
