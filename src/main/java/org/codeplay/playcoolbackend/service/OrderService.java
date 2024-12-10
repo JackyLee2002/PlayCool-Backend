d817213b4d6d25869955b6bc43bef1fe5bccded8
Mason ma <MAMA14@oocl.com>
2024-12-10 10:38:36 +0800

    refactor:use seat area and venue repository

--- src/main/java/org/codeplay/playcoolbackend/service/OrderService.java
+++ src/main/java/org/codeplay/playcoolbackend/service/OrderService.java
@@ -4,11 +4,13 @@ import org.codeplay.playcoolbackend.dto.OrderRequestDto;
 import org.codeplay.playcoolbackend.dto.OrderResponseDto;
 import org.codeplay.playcoolbackend.dto.PaymentRequestDto;
 import org.codeplay.playcoolbackend.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface OrderService {
-    List<OrderResponseDto> getOrdersByUserId(Long userId);
+    Page<OrderResponseDto> getOrdersByUserId(Long userId, Pageable pageable);
 
     OrderResponseDto createOrder(OrderRequestDto orderRequestDto);
 
